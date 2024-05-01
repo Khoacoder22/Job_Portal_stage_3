ec25a113152c536b80fbea7a5457ae01676c6495
hello_khoa <118266027+Khoacoder22@users.noreply.github.com>
2024-05-01 19:00:52 +0700

    new one

---
+++ OnlineJobPortal-20240414T061316Z-001/OnlineJobPortal/OnlineJobPortal/User/Profile.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineJobPortal.User
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        SqlDataAdapter sda;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            if (!IsPostBack)
+            {
+                showUserProfile();
+
+            }
+        }
+
+        private void showUserProfile()
+        {
+            try
+            {
+                con = new SqlConnection(str);
+                // Sử dụng tên stored procedure
+                cmd = new SqlCommand("GetUserProfile", con);
+                // Đặt CommandType là StoredProcedure
+                cmd.CommandType = CommandType.StoredProcedure; 
+                cmd.Parameters.AddWithValue("@UserName", Session["user"]);
+                con.Open();
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dlProfile.DataSource = dt;
+                dlProfile.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void dlProfile_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if(e.CommandName == "EditUserProfile")
+            {
+                Response.Redirect("Resume.aspx?id=" + e.CommandArgument.ToString());
+            }
+        }
+    }
+}
